00d83f83f4225bca8180657b5e13ae79e980677f
Avanish Ganapathy <167166972+Git-avi12@users.noreply.github.com>
2026-04-28 19:53:37 +0530

    Add: sum-of-two-integers - submission-6

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-6.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b!=0){
+            int temp1 = a^b;
+            int temp2 = (a&b)<<1;
+            a = temp1;
+            b = temp2;
+        }
+        return a;
+        
+    }
+};
