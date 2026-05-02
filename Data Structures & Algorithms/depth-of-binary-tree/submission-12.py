555bd5cab1f80146a9ad0abe333ad34920c13a44
Avanish Ganapathy <167166972+Git-avi12@users.noreply.github.com>
2026-05-02 21:53:54 +0530

    Add: depth-of-binary-tree - submission-12

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-12.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#iterative BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        q = collections.deque()
+        q.append(root)
+        count=0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            count+=1
+        return count
